e7e918a8c01f841b8edfe4478ee0817a828f290e
Shilpi sinha <shilpisinha@Shilpis-MacBook-Air.local>
2020-09-12 11:21:01 +0530

    3rd commit

--- R script.R
+++ R script.R
@@ -5,4 +5,19 @@ summary(Groceries)
 str(Groceries)
 inspect(Groceries)
 ?apriori
-gr_rules <- apriori(Groceries)
+gr_rules <- apriori(Groceries, parameter = list(supp = .001, conf = .8))
+inspect (gr_rules[1:10])
+gr_rules <- sort(gr_rules , by = "support", decreasing = T )
+inspect (gr_rules[1:10])
+gr_rules
+redundant_rules <- is.redundant(gr_rules)
+redundant_rules
+summary(redundant_rules)
+gr_rules <- gr_rules[!redundant_rules]
+gr_rules
+inspect (gr_rules[1:10])
+gr_rules
+inspect (gr_rules[1:10])
+gr_rules_beer <- apriori(Groceries, parameter = list(supp= .0001, conf = .08), appearance = list(default = "rhs", lhs= "bottled beer")) 
+inspect (gr_rules_beer[1:10])
+plot(gr_rules, method = "graph", interactive = T)
